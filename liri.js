d4548ed6379372e71cfd1bfd6f054b2e73bbec91
morralex <morr.alex@outlook.com>
2019-07-28 22:06:36 -0700

    OMDB is up

--- liri.js
+++ liri.js
@@ -7,27 +7,45 @@ var Spotify = require('node-spotify-api');
 
 //------ variables for user input -------------//
 
-// var selection = process.argv[2];
-var userInput = process.argv.slice(2).join(" ");
-
-
-// lets get the Spotify from the npm documentation ----------------//
-
+var selection = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
 
 var spotify = new Spotify(keys.spotify)
 
-spotify.search({ type: 'track', query: userInput }, function (err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+//------------------------------------Building the if statements for SPOTIFY search----------------------------------//
 
-  console.log("\nArtist: " + data.tracks.items[0].artists[0].name)
-  console.log("Song: " + data.tracks.items[0].name)
-  console.log("Link to song: " + data.tracks.items[0].external_urls.spotify)
-  console.log("Album: " + data.tracks.items[0].album.name + "\n")
-});
+if (selection === 'spotify-this-song') {
 
-// Lets get search for a movie with the OMDB API key
+  //------------------------------------SPOTIFY SEARCH------------------------------------//
 
-// var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
-//  axios.get(queryURL)
+  spotify.search({ type: 'track', query: userInput }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    console.log("\nArtist: " + data.tracks.items[0].artists[0].name)
+    console.log("Song: " + data.tracks.items[0].name)
+    console.log("Link to song: " + data.tracks.items[0].external_urls.spotify)
+    console.log("Album: " + data.tracks.items[0].album.name + "\n")
+  });
+
+}
+
+
+//------------------------------------Building the if statements for OMDB search----------------------------------//
+
+if (selection === "movie-this") {
+  var queryURL = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+  axios.get(queryURL)
+    .then(function (response){
+  console.log("\nTitle: " + response.data.Title)
+  console.log("Year: " + response.data.Year)
+  console.log("IMDB Rating: " + response.data.Ratings[0].Value)
+  console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+  console.log("Country: " + response.data.Country)
+  console.log("Language: " + response.data.Language)
+  console.log("Plot: " + response.data.Plot)
+  console.log("Actors: " + response.data.Actors + "\n")
+    })
+
+}
